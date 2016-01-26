82369b294323c44ce0078d64ab8b04cca202a88b
Simon Funke <simon.funke@gmail.com>
2016-01-26 14:38:49 +0000

    Remove unused code

--- tests_dolfin/expression_derivative_inline_cpp/expression_derivative_inline_cpp.py
+++ tests_dolfin/expression_derivative_inline_cpp/expression_derivative_inline_cpp.py
@@ -2,23 +2,6 @@ from dolfin import *
 from dolfin_adjoint import *
 
 
-# An expression that depends on a and b
-#cpp_code = '''
-#class MyCppExpression : public Expression
-#{
-#public:
-#  MyCppExpression() : Expression(), a(0), b(0) { }
-#
-#  void eval(Array<double>& values, const Array<double>& x) const
-#  {
-#    const double dx = x[0] - a;
-#    values[0] = dx*b;
-#  }
-#public:
-#    double a, b;
-#};'''
-
-
 if __name__ == "__main__":
     mesh = UnitSquareMesh(4, 4)
     V = FunctionSpace(mesh, "CG", 1)
