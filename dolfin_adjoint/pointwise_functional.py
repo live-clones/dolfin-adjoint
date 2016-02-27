04d353305b59061138c979dfadd8569ec54f8e63
Steven VDK <steven@vdks.be>
2016-02-27 11:54:31 +0100

    some prettyfications

--- dolfin_adjoint/pointwise_functional.py
+++ dolfin_adjoint/pointwise_functional.py
@@ -19,47 +19,47 @@ class PointwiseFunctional(functional.Functional):
     # refs : vector containing the observations
     # coord : coordinates of the point in which the functional has to be evaluated
     # times: list of the time instants that are considered in the functional
-    # f_ind: the inde of the component of u that is considered
+    # u_ind: the inde of the component of u that is considered
     # timeform: ??
     # verbose : T/F Spill out info while running
     # name: allow to set a name, which is usefull for all da obejcts
 
     #-----------------------------------------------------------------------------------------------------
-    def __init__(self, u, refs, coords, times=None, f_ind=None, timeform=False, verbose=False, name=None):
-        if times is None:
-            times = ["FINISH_TIME"]
-        elif len(times) < 1:
+    def __init__(self, u, refs, coords, times=None, **kwargs):
+
+        # Sort out input params
+        self.coords   = coords    # Numpy array with coordinates
+        self.func     = u         # Dolfin function to be evaluated
+        self.refs     = refs      # References
+        self.times    = times     # Relevant times
+        self.timeform = kwargs.get("timeform", False)
+        self.verbose  = kwargs.get("verbose",  False)
+        self.name     = kwargs.get("name", None)
+        self.regform  = kwargs.get("regform", None)
+        self.boost    = kwargs.get("boost", 1.0)
+        self.index    = kwargs.get("u_ind", None)
+
+        if self.times is None:
+            self.times = ["FINISH_TIME"]
+        elif len(self.times) < 1:
             raise RuntimeError("""The 'times' argument should be None,
                                     'FINISH_TIME' or a non-empty list""")
 
         # we prepare a ghost timeform. Only the time instant is important
-        if not timeform:
-            if f_ind is None:
-                self.timeform = sum(u*dx*dt[t] for t in times)
+        if not self.timeform:
+            if self.index is None:
+                self.timeform = sum(u*dx*dt[t] for t in self.times)
             else:
-                self.timeform = sum(u[f_ind]*dx*dt[t] for t in times)
-        else: self.timeform = timeform
-
-        # Store info to object
-        self.coords = coords    # Numpy array
-        self.verbose = verbose
-        self.name = name
-        self.func = u
-        self.refs = refs
-        self.times = times
-        self.i = f_ind
-        self.boost = 1e20
+                self.timeform = sum(u[self.index]*dx*dt[t] for t in self.times)
 
         # check compatibility inputs
-        if len(u) is not len(times):
-            raise RuntimeError("Number of timesteps and observations don't match")
+        if len(self.refs) is not len(self.times):
+            raise RuntimeError("Number of timesteps and observations doesn't match")
 
         # Prepare pointwise evals for derivative
-        ps = backend.PointSource(self.func.function_space().sub(self.i), backend.Point(self.coords), 1.)
+        ps = backend.PointSource(self.func.function_space().sub(self.index), backend.Point(self.coords), 1.)
         self.basis = backend.Function(self.func.function_space()) # basis function for R
         ps.apply(self.basis.vector())
-
-        if self.verbose: print "basis = ", self.basis
 
         # Failsaife for parallel
         if sum(self.basis.vector().array())<1.:
@@ -77,20 +77,20 @@ class PointwiseFunctional(functional.Functional):
             if timestep is adjointer.timestep_count -1:
 
                 # add final contribution
-                if self.i is None: solu = values[0].data(self.coords)
-                else: solu = values[0].data[self.i](self.coords)
+                if self.index is None: solu = values[0].data(self.coords)
+                else: solu = values[0].data[self.index](self.coords)
                 ref  = self.refs[self.times.index(self.times[-1])]
                 my = (solu - float(ref))*(solu - float(ref))
 
                 # if necessary, add one but last contribution
                 if toi in self.times and len(values) > 0:
-                    if self.i is None: solu = values[-1].data(self.coords)
-                    else: solu = values[-1].data[self.i](self.coords)
+                    if self.index is None: solu = values[-1].data(self.coords)
+                    else: solu = values[-1].data[self.index](self.coords)
                     ref  = self.refs[self.times.index(toi)]
                     my += (solu - float(ref))*(solu - float(ref))
             else:
-                if self.i is None: solu = values[-1].data(self.coords)
-                else: solu = values[-1].data[self.i](self.coords)
+                if self.index is None: solu = values[-1].data(self.coords)
+                else: solu = values[-1].data[self.index](self.coords)
                 ref  = self.refs[self.times.index(toi)]
                 my = (solu - float(ref))*(solu - float(ref))
         else:
@@ -135,8 +135,8 @@ class PointwiseFunctional(functional.Functional):
                     ind = 1
             coef = values[ind].data
             ref  = self.refs[self.times.index(toi)]
-            if self.i is None: solu = coef(self.coords)
-            else: solu = coef[self.i](self.coords)
+            if self.index is None: solu = coef(self.coords)
+            else: solu = coef[self.index](self.coords)
             ff = backend.Constant(self.boost*2.0*(solu - float(ref)))
             v = backend.project(ff*self.basis, self.func.function_space())
 
