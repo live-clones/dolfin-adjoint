7f89cbb573bf53f569197eb8204c694ce1d41c21
David Ham <david.ham@imperial.ac.uk>
2016-03-02 11:32:54 +0000

    TAO integration does not support firedrake

--- dolfin_adjoint/ui.py
+++ dolfin_adjoint/ui.py
@@ -30,10 +30,10 @@ from optimization.optimization_problem import *
 from optimization.optimization_solver import *
 from optimization.ipopt_solver import *
 from optimization.optizelle_solver import *
-from optimization.tao_solver import TAOSolver
 from optimization.riesz_maps import *
 
 if backend.__name__ == "dolfin":
+    from optimization.tao_solver import TAOSolver
     from newton_solver import NewtonSolver
     from krylov_solver import KrylovSolver
     from linear_solver import LinearSolver
