1920d9b56df350760327e2db8dbbbe1b804fd89a
Simon Funke <simon.funke@gmail.com>
2016-08-31 15:20:56 +0200

    Fix optimization_pyipopt test

--- tests_dolfin/optimization_pyipopt/optimization_pyipopt.py
+++ tests_dolfin/optimization_pyipopt/optimization_pyipopt.py
@@ -52,7 +52,7 @@ def derivative_cb(j, dj, m):
 if __name__ == "__main__":
 
     ic = project(Expression("sin(2*pi*x[0])"),  V)
-    u = ic.copy(deepcopy=True, name='Velocity')
+    u = Function(V)
 
     J = Functional(u*u*dx*dt[FINISH_TIME])
 
