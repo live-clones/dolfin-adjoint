0b2c61ab1575ef0f47b0aaba0ca6a3edfc0efa26
Simon Funke <simon.funke@gmail.com>
2016-09-01 10:25:36 +0200

    Add a conftest

---
+++ tests_dolfin/conftest.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import random
+import pytest
+import dolfin
+import dolfin_adjoint
+
+default_params = dolfin.parameters.copy()
+def pytest_runtest_setup(item):
+    """ Hook function which is called before every test """
+
+    # Reset dolfin parameter dictionary
+    dolfin.parameters.update(default_params)
+
+    # Reset adjoint state
+    dolfin_adjoint.adj_reset()
+
+    # Fix the seed to avoid random test failures due to slight tolerance variations
+    random.seed(21)
