d4d330b01b31835388a7b278fc41041faf6a097a
Simon Funke <simon.funke@gmail.com>
2016-09-26 18:17:15 +0200

    Switch to mathjax

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -77,8 +77,8 @@ needs_sphinx = '1.1'
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
-        'sphinx.ext.coverage', 'sphinx.ext.imgmath', 'sphinxcontrib.bibtex']
-imgmath_image_format = 'svg'
+        'sphinx.ext.coverage', 'sphinx.ext.mathjax', 'sphinxcontrib.bibtex']
+#imgmath_image_format = 'svg'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
