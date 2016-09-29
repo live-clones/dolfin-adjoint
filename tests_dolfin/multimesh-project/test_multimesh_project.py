7b3c2d151347e4ea0833ebc507c784644cb8db96
Simon Funke <simon.funke@gmail.com>
2016-09-29 14:05:37 +0200

    Test projection with multimesh

---
+++ tests_dolfin/multimesh-project/test_multimesh_project.py
@@ -0,0 +1,26 @@
+from dolfin import *
+from dolfin_adjoint import *
+
+def test_project():
+    mesh_0 = UnitSquareMesh(10, 10)
+    multimesh = MultiMesh()
+    multimesh.add(mesh_0)
+    multimesh.build()
+
+    # Define functionspace
+    V = MultiMeshFunctionSpace(multimesh, "Lagrange", 1)
+
+    g = MultiMeshFunction(V)
+    g.vector()[:] = 1
+    u0 = project(g, V, name="u0", annotate=True)
+
+    J = Functional(u0**3*dX)
+    m = Control(g)
+
+    rf = ReducedFunctional(J, m)
+    rf.taylor_test(g)
+
+if __name__ == '__main__':
+    test_project()
+
+
