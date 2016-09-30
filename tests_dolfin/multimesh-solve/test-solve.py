3824ec40f7de894461e444e26535daa1100603f8
Simon Funke <simon.funke@gmail.com>
2016-09-30 09:31:11 +0200

    Add a test for multimesh solve

---
+++ tests_dolfin/multimesh-solve/test-solve.py
@@ -0,0 +1,62 @@
+from dolfin import *
+from dolfin_adjoint import *
+
+class Dirichlet(SubDomain):
+    def inside(self, x, on_boundary):
+        return on_boundary
+
+def test_move():
+    mesh_0 = UnitSquareMesh(10, 10)
+    multimesh = MultiMesh()
+    multimesh.add(mesh_0)
+    multimesh.build()
+
+    # Define functionspace
+    V = MultiMeshFunctionSpace(multimesh, "Lagrange", 1)
+
+    # Define trial and test functions and right-hand side
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    u0 = MultiMeshFunction(V)
+    u0.vector()[:] = 1
+    u1 = MultiMeshFunction(V, name="u1")
+    f = MultiMeshFunction(V)
+
+    # Define facet normal and mesh size
+    n = FacetNormal(multimesh)
+    h = 2.0*Circumradius(multimesh)
+    h = (h('+') + h('-')) / 2
+
+    # Set parameters
+    alpha = 4.0
+    beta = 4.0
+    dt = 0.1
+
+    # Define bilinear form
+    a = u*v*dX + dt* ( inner(grad(u), grad(v))*dX \
+        - dot(avg(grad(u)), jump(v, n))*dI \
+        - dot(avg(grad(v)), jump(u, n))*dI \
+        + alpha/h*jump(u)*jump(v)*dI \
+        + beta*dot(jump(grad(u)), jump(grad(v)))*dO )
+
+    # Define linear form
+    L= u0*v*dX + dt*f*v*dX
+
+    # Assemble linear system
+    A = assemble_multimesh(a)
+    b = assemble_multimesh(L)
+
+    # Solving linear system
+    bound = Dirichlet()
+    bc = MultiMeshDirichletBC(V, Constant(0), bound)
+    bc.apply(A,b)
+    solve(A, u1.vector(), b)
+
+    J = Functional(u1**2*dX)
+    m = [Control(u0), Control(f)]
+    rf = ReducedFunctional(J, m)
+    rf.taylor_test([u0, f])
+
+
+if __name__ == '__main__':
+    test_move()
