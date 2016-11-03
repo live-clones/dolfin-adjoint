9766086f368c2644fee9acc9ddd39258452551a7
Simon Funke <simon.funke@gmail.com>
2016-11-03 17:34:38 +0100

    Start a script for creating new releases

---
+++ scripts/new_release.sh
@@ -0,0 +1,8 @@
+# This is a stub for the steps required to create a new release
+
+echo "Creating release tar ball"
+PACKAGE=dolfin-adjoint
+VERSION=2016.1.0
+ARCHIVE=$PACKAGE-$VERSION.tar.gz
+git archive --prefix=$PACKAGE-$VERSION -o $ARCHIVE $PACKAGE-$VERSION
+
