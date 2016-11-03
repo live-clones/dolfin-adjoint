dbc73957d65ca269d0866e3e5594c7c25b7db5bf
Simon Funke <simon.funke@gmail.com>
2016-11-03 17:36:13 +0100

    Minor fixes

--- scripts/new_release.sh
+++ scripts/new_release.sh
@@ -4,5 +4,5 @@ echo "Creating release tar ball"
 PACKAGE=dolfin-adjoint
 VERSION=2016.1.0
 ARCHIVE=$PACKAGE-$VERSION.tar.gz
-git archive --prefix=$PACKAGE-$VERSION -o $ARCHIVE $PACKAGE-$VERSION
+git archive --prefix=$PACKAGE-$VERSION/ -o $ARCHIVE $PACKAGE-$VERSION
 
