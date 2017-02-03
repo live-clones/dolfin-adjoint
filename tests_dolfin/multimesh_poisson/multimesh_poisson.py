bcccefbd26b3eb8f3ae6d1b8cb46f759182916be
jorgensd <dokken92@gmail.com>
2017-02-03 15:45:33 +0100

    Started on creating real optimization multimesh tests, works with dolfin, not with scipy

--- tests_dolfin/multimesh_poisson/multimesh_poisson.py
+++ tests_dolfin/multimesh_poisson/multimesh_poisson.py
@@ -1,42 +1,37 @@
 from dolfin import *
 from dolfin_adjoint import *
-#import moola
 
-
-
-class Noslip(SubDomain):
+class Boundary(SubDomain):
     def inside(self, x, on_boundary):
         return on_boundary
 
-
-def solve_poisson():
-    # Creating two intersecting meshes
-    #mesh_0 = RectangleMesh(Point(-1.5, -0.75), Point(1.5, 0.75), 40, 20)
-    mesh_0 = RectangleMesh(Point(0,0), Point(1,1), 20, 20) 
-    mesh_1 = RectangleMesh(Point(0.55,0.1),  Point(0.9,0.85), 25 ,40)
-    # mesh_2 = RectangleMesh(Point(-1.5,-1.5),  Point(0,0),10,10)
+def solve_poisson_moola(N,element,degree):
+    # Creating mesh
+    import moola
+    mesh_0 = RectangleMesh(Point(0,0), Point(1,1), N,N)
+    mesh_1 = RectangleMesh(Point(0.25, 0.25),  Point(0.75, 0.75), N,N)
     multimesh = MultiMesh()
     multimesh.add(mesh_0)
     multimesh.add(mesh_1)
-    # multimesh.add(mesh_2)
     multimesh.build()
 
     # Create function space for the temperature
-    V = MultiMeshFunctionSpace(multimesh, "Lagrange", 1)
+    V = MultiMeshFunctionSpace(multimesh, "CG", 1)
+    W = MultiMeshFunctionSpace(multimesh, element, degree)
 
-    # Create a function space for control-function f
-    #W = MultiMeshFunctionSpace(multimesh, 'DG', 0)
+    # Initialize boundary condition on state
+    boundary = Boundary()
+    bc0 = MultiMeshDirichletBC(V, Constant(0), boundary)
 
-    # Define intial guess for controll-function f (This could be zero)
-    # Issue 675: We can't interpolate f into a multimeshfunctionspace
-    #f = interpolate(Expression('x[0]+x[1]', degree=1), W, name='Control')
-    # f = Constant(1)
-
+    # Initial guess for source distribution
+    bcf = MultiMeshDirichletBC(W, Constant(0), boundary)
+    fex = Expression('x[0]+x[1]',degree=1)
+    f = project(fex, W, name="Control")
+    bc0.apply(f.vector())
 
     # Define trial and test functions and right-hand side
     u = TrialFunction(V)
     v = TestFunction(V)
-
 
     # Define facet normal and mesh size
     n = FacetNormal(multimesh)
@@ -47,45 +42,141 @@ def solve_poisson():
     alpha = 4.0
     beta = 4.0
 
-    # Define bilinear form,
+    # Define bilinear form and linear form
     a = dot(grad(u), grad(v))*dX \
         - dot(avg(grad(u)), jump(v, n))*dI \
         - dot(avg(grad(v)), jump(u, n))*dI \
-        + alpha/h*jump(u)*jump(v)*dI \
-        + beta*dot(jump(grad(u)), jump(grad(v)))*dO # \
-        # - (f*v)*dX
-    # Define linear form
-    L= Constant(1)*v*dX
-    # L = Constant(0)*v*dX
+        + alpha/h*jump(u)*jump(v)*dI  \
+        + beta*dot(jump(grad(u)), jump(grad(v)))*dO
+    L= f*v*dX
+
     # Assemble linear system
     A = assemble_multimesh(a)
     b = assemble_multimesh(L)
-
-    noslip=Noslip()
-    bc0 = MultiMeshDirichletBC(V, Constant(0), noslip)
-
     bc0.apply(A,b)
-    u = MultiMeshFunction(V)
-    solve(A, u.vector(), b)
     
-    # adj_html("forward.html", "forward")
-    # domains = CellFunction("size_t", multimesh)
-    J = Functional(u*u*u*dX)
-    c = Control(u)
+    # Solving linear system
+    u = MultiMeshFunction(V, name="State")
+    solve(A, u.vector(), b, 'lu')
 
-    #assemble_multimesh(u*v*dX)
-    q= compute_gradient(J, c)
-    plot(q.part(0), title='der0')
-    plot(q.part(1), title='der1')
-    plot(u.part(0), title='u0')
-    plot(u.part(1), title='u1')
-    interactive()
-    # plot(multimesh)
-    # interactive()
+    # Defining functional and desired temperature profile
+    alpha1 = Constant(1e-6)
+    x = SpatialCoordinate(multimesh)
+    d = 2*pi**2*alpha1*sin(pi*x[0])*sin(pi*x[1]) + sin(pi*x[0])*sin(pi*x[1])/(2*pi**2)
+    J = Functional(0.5*inner(u-d,u-d)*dX + alpha1/2.*f**2*dX)
+    control = Control(f)
+    rf = ReducedFunctional(J,control)
 
+    # Moola optimization
+    problem = MoolaOptimizationProblem(rf)
+    f_moola = moola.DolfinPrimalVector(f, inner_product="L2")
+    solver = moola.BFGS(problem, f_moola, options={'jtol': 1e-30,
+                                                   'rjtol':1e-30,
+                                                   'rgtol':1e-30,
+                                                   'gtol':5e-11,
+                                                   'Hinit': "default",
+                                                   'maxiter': 200,
+                                                   'mem_lim': 10})
 
+    sol = solver.solve()
+    f_opt = sol['control'].data
 
-    
-if __name__ == '__main__':
-    solve_poisson()
+    # Computing u with optimal control
+    L = f_opt*v*dX
+    b = assemble_multimesh(L)
+    bc0.apply(b)
+    solve(A, u.vector(), b, 'lu')
 
+    # Analytical solutions to problem
+    Ex = MultiMeshFunctionSpace(multimesh, "CG", 3)
+    u_analytic = Expression("1/(2*pi*pi)*sin(pi*x[0])*sin(pi*x[1])", degree=3)
+    u_analytic = project(u_analytic, Ex)
+    f_analytic = Expression("sin(pi*x[0])*sin(pi*x[1])", degree=3)
+    f_analytic = project(f_analytic, Ex)
+
+    # L2 errors
+    control_error = sqrt(assemble_multimesh((f_opt - f_analytic)**2*dX))
+    state_error = sqrt(assemble_multimesh((u - u_analytic)**2*dX))
+    return 1./N, control_error
+
+def solve_poisson_scipy(N,element,degree):
+    # Creating mesh
+    mesh_0 = RectangleMesh(Point(0,0), Point(1,1), N,N)
+    mesh_1 = RectangleMesh(Point(0.25, 0.25),  Point(0.75, 0.75), N,N)
+    multimesh = MultiMesh()
+    multimesh.add(mesh_0)
+    multimesh.add(mesh_1)
+    multimesh.build()
+
+    # Create function space for the temperature
+    V = MultiMeshFunctionSpace(multimesh, "CG", 1)
+    W = MultiMeshFunctionSpace(multimesh, element, degree)
+
+    # Initialize boundary condition on state
+    boundary = Boundary()
+    bc0 = MultiMeshDirichletBC(V, Constant(0), boundary)
+
+    # Initial guess for source distribution
+    bcf = MultiMeshDirichletBC(W, Constant(0), boundary)
+    fex = Expression('x[0]+x[1]',degree=1)
+    f = project(fex, W, name="Control")
+    bc0.apply(f.vector())
+
+    # Define trial and test functions and right-hand side
+    u = TrialFunction(V)
+    v = TestFunction(V)
+
+    # Define facet normal and mesh size
+    n = FacetNormal(multimesh)
+    h = 2.0*Circumradius(multimesh)
+    h = (h('+') + h('-')) / 2
+
+    # Set parameters
+    alpha = 4.0
+    beta = 4.0
+
+    # Define bilinear form and linear form
+    a = dot(grad(u), grad(v))*dX \
+        - dot(avg(grad(u)), jump(v, n))*dI \
+        - dot(avg(grad(v)), jump(u, n))*dI \
+        + alpha/h*jump(u)*jump(v)*dI  \
+        + beta*dot(jump(grad(u)), jump(grad(v)))*dO
+    L= f*v*dX
+
+    # Assemble linear system
+    A = assemble_multimesh(a)
+    b = assemble_multimesh(L)
+    bc0.apply(A,b)
+
+    # Solving linear system
+    u = MultiMeshFunction(V, name="State")
+    solve(A, u.vector(), b, 'lu')
+
+    # Defining functional and desired temperature profile
+    alpha1 = Constant(1e-6)
+    x = SpatialCoordinate(multimesh)
+    d = 2*pi**2*alpha1*sin(pi*x[0])*sin(pi*x[1]) + sin(pi*x[0])*sin(pi*x[1])/(2*pi**2)
+    J = Functional(0.5*inner(u-d,u-d)*dX + alpha1/2.*f**2*dX)
+    control = Control(f)
+    rf = ReducedFunctional(J,control)
+
+    # Scipy optimization
+    f_opt = minimize(rf, tol=1e-16, options={"ftol":1e-15})
+
+    # Computing u with optimal control
+    L = f_opt*v*dX
+    b = assemble_multimesh(L)
+    bc0.apply(b)
+    solve(A, u.vector(), b, 'lu')
+
+    # Analytical solutions to problem
+    Ex = MultiMeshFunctionSpace(multimesh, "CG", 3)
+    u_analytic = Expression("1/(2*pi*pi)*sin(pi*x[0])*sin(pi*x[1])", degree=3)
+    u_analytic = project(u_analytic, Ex)
+    f_analytic = Expression("sin(pi*x[0])*sin(pi*x[1])", degree=3)
+    f_analytic = project(f_analytic, Ex)
+
+    # L2 errors
+    control_error = sqrt(assemble_multimesh((f_opt - f_analytic)**2*dX))
+    state_error = sqrt(assemble_multimesh((u - u_analytic)**2*dX))
+    return 1./N, control_error
--- tests_dolfin/multimesh_poisson/test_multimesh_poisson.py
+++ tests_dolfin/multimesh_poisson/test_multimesh_poisson.py
@@ -1,12 +1,59 @@
-from os import path
-import subprocess
+# from os import path
+# import subprocess
 import pytest
+import numpy as np
+from dolfin_adjoint import *
+from multimesh_poisson import *
+# @pytest.mark.skip("Not supported by dolfin master yet")
+# def test(request):
+#     test_file = path.split(path.dirname(str(request.fspath)))[1] + ".py"
+#     test_dir = path.split(str(request.fspath))[0]
+#     test_cmd = ["python", path.join(test_dir, test_file)]
 
-@pytest.mark.skip("Not supported by dolfin master yet")
-def test(request):
-    test_file = path.split(path.dirname(str(request.fspath)))[1] + ".py"
-    test_dir = path.split(str(request.fspath))[0]
-    test_cmd = ["python", path.join(test_dir, test_file)]
+#     handle = subprocess.Popen(test_cmd, cwd=test_dir)
+#     assert handle.wait() == 0
 
-    handle = subprocess.Popen(test_cmd, cwd=test_dir)
-    assert handle.wait() == 0
+def test_multimesh_poisson_cg_moola():
+    moola = pytest.importorskip("moola", minversion="0.1")
+    Ns = [33,65]
+    h, control = np.zeros(2), np.zeros(2)
+    for i in range(2):
+        adj_reset()
+        h[i],control[i] = solve_poisson_moola(Ns[i],"CG", 1)
+    rate = np.log(control[1]/control[0])/np.log(h[1]/h[0])
+    exp_rate = 2
+    assert(rate>exp_rate-0.05)
+
+def test_multimesh_poisson_dg_moola():
+    moola = pytest.importorskip("moola", minversion="0.1")
+    Ns = [33,65]
+    h, control = np.zeros(2), np.zeros(2)
+    for i in range(2):
+        adj_reset()
+        h[i],control[i] = solve_poisson_moola(Ns[i],"DG", 0)
+    rate = np.log(control[1]/control[0])/np.log(h[1]/h[0])
+    exp_rate = 1
+    assert(rate>exp_rate-0.05)
+
+@pytest.mark.skipif(True, 'Not working yet')
+def test_multimesh_poisson_cg_scipy():
+    moola = pytest.importorskip("scipy")
+    Ns = [33,65]
+    h, control = np.zeros(2), np.zeros(2)
+    for i in range(2):
+        adj_reset()
+        h[i],control[i] = solve_poisson_scipy(Ns[i],"CG", 1)
+    rate = np.log(control[1]/control[0])/np.log(h[1]/h[0])
+    exp_rate = 2
+    assert(rate>exp_rate-0.05)
+
+def test_multimesh_poisson_dg_scipy():
+    moola = pytest.importorskip("scipy")
+    Ns = [33,65]
+    h, control = np.zeros(2), np.zeros(2)
+    for i in range(2):
+        adj_reset()
+        h[i],control[i] = solve_poisson_scipy(Ns[i],"DG", 0)
+    rate = np.log(control[1]/control[0])/np.log(h[1]/h[0])
+    exp_rate = 1
+    assert(rate>exp_rate-0.05)
