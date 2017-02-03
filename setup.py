7dd39c0ce190ef4019dbc31e2d60fb49cfe949b7
jorgensd <dokken92@gmail.com>
2017-02-03 11:22:55 +0100

    Adding libadjoint requirements for installation

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
-
+VERSION = '2016.2.0'
 setup (name = 'dolfin_adjoint',
-       version = '2016.1.0',
+       version = VERSION,
        description = 'Automatically derive the discrete adjoint of FEniCS and Firedrake models',
        author = 'The dolfin_adjoint team',
        author_email = 'patrick.farrell@maths.ox.ac.uk and simon@simula.no',
@@ -9,4 +9,5 @@ setup (name = 'dolfin_adjoint',
                    'firedrake_adjoint'],
        package_dir = {'dolfin_adjoint': 'dolfin_adjoint',
                       'fenics_adjoint': 'fenics_adjoint',
-                      'firedrake_adjoint': 'firedrake_adjoint'})
+                      'firedrake_adjoint': 'firedrake_adjoint'}
+       install_requires=["libadjoint==%s" % VERSION)
