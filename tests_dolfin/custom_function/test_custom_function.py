068020c46cce5a5bee9966a638769a778532d59e
Simon Funke <simon.funke@gmail.com>
2017-03-02 11:57:13 -0500

    Clean up the custom function test

--- tests_dolfin/custom_function/test_custom_function.py
+++ tests_dolfin/custom_function/test_custom_function.py
@@ -6,14 +6,14 @@ class CustomDolfinAdjointFunction(object):
 
     def __call__(self, infunc, annotate=None):
 
-        # Perform the forward action
+        # Perform the forward operation 
         out = Function(infunc.function_space())
         out.vector()[:] = out.vector().array()**2
 
         # Annotate the operation on the dolfin-adjoint tape
         if utils.to_annotate(annotate):
 
-            rhs = CustomDolfinAdjointFunctionRhs(infunc)
+            rhs = CustomDolfinAdjointFunctionRhs(coeffs=[infunc])
             out_dep = adjglobals.adj_variables.next(out)
 
             solving.register_initial_conditions(zip(rhs.coefficients(), rhs.dependencies()), linear=False)
@@ -30,36 +30,36 @@ class CustomDolfinAdjointFunction(object):
         return out
 
 class CustomDolfinAdjointFunctionRhs(libadjoint.RHS):
-    def __init__(self, infunc):
-        self.infunc = infunc
-        self.deps = [adjglobals.adj_variables[self.infunc]]
+    def __init__(self, coeffs):
+        self.coeffs = coeffs
+        self.deps = [adjglobals.adj_variables[c] for c in self.coeffs]
 
     def __call__(self, dependencies, values):
-        #from IPython import embed; embed()
+        """ The custom function implemented for the adjoint tape """
         out = values[0].data.copy(deepcopy=True)
         out.vector()[:] = out.vector().array()**2
         
         return adjlinalg.Vector(out)
 
     def derivative_action(self, dependencies, values, variable, contraction_vector, hermitian):
+        """ The linearsed (adjoint) custom function implemented for the adjoint tape """
         if hermitian:
+            # Required for the adjoint model
             out = values[0].data.copy(deepcopy=True)
             out.vector()[:] = 2*out.vector().array()*contraction_vector.data.vector().array()
         else:
+            # Required for the tangent linear model
             raise NotImplementedError()
         
         return adjlinalg.Vector(out)
 
     def dependencies(self):
-        # What does this custom function depend on?
+        """ Returns the symbolic dependendencies of the custom function """
         return self.deps 
 
     def coefficients(self):
-        return [self.infunc]
-
-    def __str__(self):
-        return "CustomDolfinAdjointFunction"
-
+        """ Returns the dolfin.coefficients of the custom function """
+        return self.coeffs
 
 
 def test_custom_function():
@@ -71,7 +71,6 @@ def test_custom_function():
     func = CustomDolfinAdjointFunction()
     v = func(g)
     adj_html("forward.html", "forward")
-    print v.vector().array()
 
     J = Functional(0.5 * inner(v, v) * dx)
 
